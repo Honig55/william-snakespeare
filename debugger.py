a5aaa89c96d114d8c0764b2f7ee6a013c2b4c0ad
5p4t3n907 <benarnk@web.de>
2026-06-30 10:40:42 +0200

    fix:ist nie passiert!

--- debugger.py
+++ debugger.py
@@ -5,11 +5,11 @@ class Debugger():
     active:bool = False
     show_time_stamp:bool = False
 
-    __init__(self, active:bool=False, show_time_stamp:bool=False):
+    def __init__(self, active:bool=False, show_time_stamp:bool=False):
         self.active = active
         self.show_time_stamp = show_time_stamp
 
-    dbg(self, message:str):
+    def dbg(self, message:str):
         if self.active:
             if self.show_time_stamp:
                 from datetime import datetime
@@ -17,14 +17,14 @@ class Debugger():
             else:
                 print(message)
 
-    enable(self):
+    def enable(self):
         self.active = True
 
-    disable(self):
+    def disable(self):
         self.active = False
 
-    enable_time_stamp(self):
+    def enable_time_stamp(self):
         self.show_time_stamp = True
 
-    disable_time_stamp(self):
+    def disable_time_stamp(self):
         self.show_time_stamp = False    
