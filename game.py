a122f30b488564b1a3531c18002f3b348065d25e
Honig <florianwenzel1305@gmail.com>
2026-07-02 21:51:43 +0200

    add: game.py (stud)

---
+++ game.py
@@ -0,0 +1,31 @@
+class Game:
+    
+    def __init__(self, size):
+        self.x
+        self.y
+        self.board[size][size]
+        self.hunger = 100
+
+    def set_x(x):
+        self.x = x
+    
+    def get_x():
+        return self.x
+
+    def set_y(y):
+        self.y = y
+    
+    def get_y():
+        return self.y
+
+    def get_known_board():
+        return self.board
+    
+    def edit_board(x,y,o):
+        slef.board[x][y] = o
+
+    def more_hunger():
+        self.hunger -= 1
+    
+    def get_hunger():
+        return self.hunger
