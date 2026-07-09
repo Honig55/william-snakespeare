3e6b149bf3feb422eebfd0d37732cd9d205a6ef4
5p4t3n907 <benarnk@web.de>
2026-07-09 15:02:52 +0200

    cng: args for run/train/test

--- main.py
+++ main.py
@@ -2,12 +2,55 @@
 import helpers.debugger as debugger
 import battlesnake_objects as game
 import agent
+import battlesnake_server
+import run_local_game
 
-dbg = debugger.Debugger(active=True, show_time_stamp=True)
+import sys
+import yaml
 
-game = game.Game(size=15)
 
-dbg.dbg(game.get_training_score())
-dbg.dbg(game.get_score())
+# calling convention:
+# python3 myin.py <mode> [<args>]
+# currently no args are supported, but this is a placeholder for future expansion
 
-dbg.dbg("done!")
+def test(debugger:debugger):
+    debugger.dbg("test")
+
+def train():
+    # call training
+    pass
+
+def run():
+    with open("config.yml", "r") as f:
+        config = yaml.safe_load(f)
+    port = config.get("server_port")
+
+    battlesnake_server.start_server(agent.Agent(), port)
+    
+    # call server
+    pass
+
+if __name__ == '__main__':
+    if argv is None or len(argv)<2:
+        print("No mode specified, defaulting to local")
+        exit(-1)
+    
+    args = sys.argv[1:]
+    
+    if len(args) < 1:
+        print("No mode specified, defaulting to local")
+        exit(-1)
+
+    mode = args[0] if len(args) > 0 else 'local'
+
+    match mode:
+        case 'test':
+            dbg = debugger.Debugger(active=True, show_time_stamp=True)
+            test(dbg)
+        case 'train':
+            train()
+        case 'run':
+            run()
+        case _:
+            print(f"Unknown mode: {mode}")
+            exit(-2)
